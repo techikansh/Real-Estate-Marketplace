829d6218135b22c6e09f52a7fcbbb463319503b0
techikansh <106448256+techikansh@users.noreply.github.com>
2024-10-05 00:57:19 +0530

    Create Header component!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,14 @@ import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
-
+import Header from "./components/Header";
 
 function App() {
   
 
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { IoMdSearch } from "react-icons/io";
+import { GiCrossedSabres } from "react-icons/gi";
+
+const Header = () => {
+
+    const [search, setSearch] = useState("")
+    
+    function handleSearch() {
+        console.log(search);
+    }
+  
+    return (
+    <header className="drop-shadow-lg bg-slate-800">
+      <div className="flex flex-wrap  text-center items-center p-3 space-x-4 justify-between mx-4">
+        <div className="text-2xl sm:text-3xl font-bold text-white">Estate Markt</div>
+        
+        <div className="bg-white px-4 py-1 rounded-lg flex text-center items-center">
+          <input className="outline-none w-32 md:w-64 lg:w-96 text-lg" type="text" placeholder="Search" value={search}
+          onChange = {(e) => {setSearch(e.target.value)}}/>
+          
+          {search && (
+            <GiCrossedSabres className="text-2xl mr-2" onClick={() => setSearch("")}/>
+          )}
+          <IoMdSearch className="text-2xl" onClick={() => {handleSearch()}}/>
+        </div>
+
+        <div className="flex flex-wrap justify-evenly text-white space-x-4 text-xl">
+          <div className="hidden lg:inline hover:text-gray-400">Home</div>
+          <div className="hidden lg:inline hover:text-gray-400">Über Uns</div>
+          <div className="hover:text-gray-400">Einloggen</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
