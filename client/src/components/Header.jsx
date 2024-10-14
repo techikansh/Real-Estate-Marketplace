21904551384a96c1082d7186e5a8fddea3234d81
techikansh <106448256+techikansh@users.noreply.github.com>
2024-10-15 01:01:17 +0530

    Implement Header Search Form Func.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 import { GiCrossedSabres } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
@@ -7,21 +7,39 @@ import { useSelector } from "react-redux";
 const Header = () => {
     const [search, setSearch] = useState("");
     const navigate = useNavigate();
-    const currentUser = useSelector(state => state.user.currentUser);
-    
+    const currentUser = useSelector((state) => state.user.currentUser);
 
     function handleSearch() {
         console.log(search);
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set("searchTerm", search);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
     }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTerm = urlParams.get("searchTerm");
+        setSearch(searchTerm);
+    }, [location.search]);
 
     return (
         <header className="drop-shadow-lg bg-black py-3">
             <div className="flex flex-wrap text-center items-center p-3 space-x-4 justify-between mx-4">
-                <div className="text-2xl sm:text-3xl font-bold text-white hover:cursor-pointer" onClick={() => navigate("/")}>
+                <div
+                    className="text-2xl sm:text-3xl font-bold text-white hover:cursor-pointer"
+                    onClick={() => navigate("/")}
+                >
                     Estate Markt
                 </div>
 
-                <div className="bg-white px-4 py-1 rounded-lg flex text-center items-center">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSearch();
+                    }}
+                    className="bg-white px-4 py-1 rounded-lg flex text-center items-center"
+                >
                     <input
                         className="outline-none w-32 md:w-64 lg:w-96 text-lg"
                         type="text"
@@ -44,7 +62,7 @@ const Header = () => {
                             handleSearch();
                         }}
                     />
-                </div>
+                </form>
 
                 <div className="flex flex-wrap items-center justify-evenly text-white space-x-4 text-xl font-semibold">
                     <div className="hidden lg:inline hover:text-gray-400 hover:cursor-pointer">
